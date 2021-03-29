c95137efa0a4683e18e1b1a7114252149be7caf6
John Joen Lim <jaicelim@gmail.com>
2021-03-29 13:14:51 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Chowder",
+      animal: "Dog",
+      breed: "Chow-Chow",
+    }),
+    React.createElement(Pet, {
+      name: "Sheldon",
+      animal: "Cat",
+      breed: "Siamese",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
