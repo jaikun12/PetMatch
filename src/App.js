91c8476a60eaf76ebea3fa83861ed21bcdbcb424
John Joen Lim <jaicelim@gmail.com>
2021-04-03 16:23:41 +0800

    Added Details.js as a Class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import { render } from "react-dom";
 import { StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
-//import Pet from "./Pet";
-
-
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-// };
 
 const App = () => {
   return (
       <div>
-          <h1>Adopt Me</h1>
-          {/* <Pet name="Luna" animal="Dog" breed="Havanese"/>
-          <Pet name="Chowder" animal="Dog" breed="Chow-Chow"/>
-          <Pet name="Sheldon" animal="Cat" breed="Siamese"/> */}
-          <SearchParams />;
-          {/* <SearchParams />;
-          <SearchParams />; */}
+          <Router>
+              <header>
+                <Link to="/">Adopt Me</Link>
+              </header>
+              <Switch>
+                <Route path="/details/:id">
+                  <Details/>
+                </Route>
+                <Route path="/">
+                  <SearchParams />
+                </Route>
+              </Switch>
+          </Router>;
       </div>
   );
 };
